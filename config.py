e45bc419eb5345f85cd74367968f5624959db1c8
Nethmifonseka97 <66138027+Nethmifonseka97@users.noreply.github.com>
2021-04-21 11:08:27 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+import tweepy
+
+consumer_key = 'RqXwrJHDrJj3HdGfrn0x4IswX'
+consumer_secret = '5YStxsGUlcKGqeVwzGQvqtUC7SOska1CfT7HGz3e9MDlyUCcOL'
+access_token = '1377692090096709632-4na2oK5zvnoUbGScSX49hSss2tXitK'
+access_token_secret = 'tHhHgcZAj2Z1ESzpwVIhPUqqggrSbwHPanwuMWjlGkIE4'
